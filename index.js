13e5ee15bec8d0942c1c3850afb4878f9c3b8d27
alec <alec@petros.audio>
2017-11-29 03:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
